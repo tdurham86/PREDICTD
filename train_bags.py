2a7d74da587b3ba2a80e70551c9eb398cfa18e19
tdurham86 <timothy.durham86@gmail.com>
2017-12-12 10:48:13 -0800

    fixed train_bags.py to use all validation sets and to properly restart if some validation sets have already had models trained for them

--- train_bags.py
+++ train_bags.py
@@ -18,7 +18,7 @@ import train_model as spark_model
 spark_model.pl.s3_library = s3_library
 #spark_model.pl.azure_library = azure_library
 
-num_folds = 2
+num_folds = 8
 
 if __name__ == "__main__":
     parser = spark_model.parser
@@ -43,9 +43,11 @@ if __name__ == "__main__":
     active_fold_path = os.path.join(out_root, 'active_fold_idx.pickle')
     try:
         if spark_model.pl.STORAGE == 'S3':
-            range_start = s3_library.get_pickle_s3(args.run_bucket, active_fold_path)
-#        elif spark_model.pl.STORAGE == 'BLOB':
-#            range_start = azure_library.get_blob_pickle(args.run_bucket, active_fold_path)
+#            range_start = s3_library.get_pickle_s3(args.run_bucket, active_fold_path)
+##        elif spark_model.pl.STORAGE == 'BLOB':
+##            range_start = azure_library.get_blob_pickle(args.run_bucket, active_fold_path)
+            range_start = len(s3_library.glob_keys(args.run_bucket,
+                                                   os.path.join(out_root, 'valid_fold*/ct_factors.pickle')))
     except:
         range_start = 0
 
